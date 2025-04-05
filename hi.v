ef7c2917c7b04d3918adcddf52c421ae0fb43e21
shampeyndadi <charlesxxavier30@gmail.com>
2025-04-06 03:10:23 +0800

    getting somewhere

--- hi.v
+++ hi.v
@@ -1,6 +1,6 @@
 `timescale 1ns/1ps
 
-module FF_T(Q,T, clk);
+module FF_T(Q,T,clk, rst);
     input T, clk, rst;
     output reg Q;
 
@@ -9,6 +9,22 @@ module FF_T(Q,T, clk);
             Q <= Q
         else 
             Q <= !Q
+endmodule
+
+module ex1(A,B,C,D,Y,reset, clock);
+    output A,B,C,D;
+    input Y, reset, clock;
+    wire TA, TB, TC, TD;
+
+    assign TA = C&D
+    assign TB = D;
+    assign TC = 1;
+    assign TD = 0;
+
+    FF_T F1(A, TA, clock, reset);
+    FF_T F2(B, TB, clock, reset);
+    FF_T F3(C, TC, clock, reset);
+    FF_T F4(D, TD, clock, reset);
 endmodule
 
 
