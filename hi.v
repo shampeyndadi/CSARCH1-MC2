1cb669eabcf7c3ec1dfead318a9e371ba1942bbb
shampeyndadi <charlesxxavier30@gmail.com>
2025-04-06 02:01:39 +0800

    T flip-flop ??

--- hi.v
+++ hi.v
@@ -1,1 +1,12 @@
 `timescale 1ns/1ps
+
+module FF_T (Q,T, clk);
+    input T, clk;
+    output reg Q;
+
+    always @ (posedge clk)
+        if (T==0)
+            Q <= Q
+        else 
+            Q <= !Q
+
